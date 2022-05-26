07fc11be5884329ef48c9a5ff95d515087ffbc82
PoZmroku <lavlahen9482@gmail.com>
2022-05-27 00:52:19 +0500

    Some fix

--- app/src/main/java/com/awesome/aviapp/Delete_Flight_Ticket.java
+++ app/src/main/java/com/awesome/aviapp/Delete_Flight_Ticket.java
@@ -3,7 +3,9 @@ package com.awesome.aviapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,7 +27,6 @@ public class Delete_Flight_Ticket extends AppCompatActivity {
     private Button delete_ticket;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     DocumentReference bookingRef;
-    DocumentReference emailRef;
     DocumentReference bankRef;
 
     @Override
@@ -46,7 +47,6 @@ public class Delete_Flight_Ticket extends AppCompatActivity {
                 {
                     id = Integer.parseInt(booking_id.getText().toString().trim());
                     bookingRef = db.collection(booking_id.getText().toString().trim()).document(name.getText().toString().trim());
-                    emailRef = db.collection(booking_id.getText().toString().trim()).document("email");
                     bankRef = db.collection("Bank").document("bank");
                     final Passenger passenger = new Passenger();
                     bookingRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
@@ -58,6 +58,31 @@ public class Delete_Flight_Ticket extends AppCompatActivity {
                                     if (document.getString("status").equals("Cancelled"))
                                         Toast.makeText(Delete_Flight_Ticket.this, "Ticket Already Cancelled", Toast.LENGTH_LONG).show();
                                     else {
+
+                                        final Bank bank = new Bank();
+                                        bank.setNo2(document.getString("price"));
+                                        Map<String, Object> map = new HashMap<>();
+                                        map.put("status", "Cancelled");
+                                        bookingRef.update(map)
+                                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                    @Override
+                                                    public void onSuccess(Void aVoid) {
+                                                        Toast.makeText(Delete_Flight_Ticket.this, "Ticket Successfully Cancelled.", Toast.LENGTH_LONG).show();
+                                                    }
+                                                });
+
+                                        bankRef.get()
+                                                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                                                    @Override
+                                                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                                        bank.setNo1(documentSnapshot.getString("amount"));
+                                                        String new_amount = bank.sub();
+                                                        Map<String, Object> map1 = new HashMap<>();
+                                                        map1.put("amount", new_amount);
+                                                        bankRef.update(map1);
+                                                    }
+                                                });
+
                                         bookingRef.get()
                                                 .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                                                     @Override
@@ -74,11 +99,17 @@ public class Delete_Flight_Ticket extends AppCompatActivity {
 
                                     }
                                 }
+                                else
+                                {
+                                    Toast.makeText(Delete_Flight_Ticket.this,"No Such Booking Exist",Toast.LENGTH_LONG).show();
+                                }
+                            }else {
+                                Log.d("Failed Deleting Booking", "Failed with: ", task.getException());
                             }
                         }
                     });
                 } catch (NumberFormatException e) {
-                    e.printStackTrace();
+                    Toast.makeText(Delete_Flight_Ticket.this,"Enter Data Correctly",Toast.LENGTH_LONG).show();
                 }
             }
         });
